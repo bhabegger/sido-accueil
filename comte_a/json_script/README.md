03b73b81dd44ebc44ca3886e8a0aa63d2efcb44d
RoToRx88 <comte_a@epitech.eu>
2015-03-12 23:27:32 +0100

    check bug

--- comte_a/json_script/README.md
+++ comte_a/json_script/README.md
@@ -1,3 +1,4 @@
+# JSON file managment
 script to get a json file from a rest api and check the validity of the json. If valid, push the 
 json getted into a file. Else keep the old json
 
--- comte_a/json_script/getJson.py
+++ comte_a/json_script/getJson.py
@@ -3,6 +3,7 @@
 import pycurl
 import os.path
 import json
+import time
 from io import BytesIO
 
 def is_json(myjson):
@@ -12,34 +13,38 @@ def is_json(myjson):
 		return False
 	return True
 
-old_json_name = "old_json.json"
-new_json_name = "new_json.json"
+old_json_name = "old.json"
+new_json_name = "new.json"
+json_url = 'http://qze.fr/colo.json'
 
-buffer = BytesIO()
-dl = pycurl.Curl()
-dl.setopt(dl.URL, 'http://google.com')
-dl.setopt(dl.WRITEDATA, buffer)
-dl.perform()
-dl.close()
-#
-# End of curling the json
-#
-body = buffer.getvalue()
-dl_json = body.decode('iso-8859-1')
-if is_json(dl_json):
-	if os.path.isfile(new_json_name):
-		new_json = open(new_json_name, 'r')
-	else:
+while True:
+#	time.sleep(0.2)
+	print ("[debug] Starting downloading", json_url)
+	buffer = BytesIO()
+	dl = pycurl.Curl()
+	dl.setopt(dl.URL, json_url)
+	dl.setopt(dl.WRITEDATA, buffer)
+	dl.perform()
+	dl.close()
+	#
+	# End of curling the json
+	#
+	body = buffer.getvalue()
+	dl_json = body.decode('iso-8859-1')
+	if is_json(dl_json):
+		if os.path.isfile(new_json_name):
+			new_json = open(new_json_name, 'r')
+		else:
+			new_json = open(new_json_name, 'w')
+			new_json.close()
+			new_json = open(new_json_name, 'r')
+		if new_json:
+			old_json = open(old_json_name, 'w')
+			old_json.write(new_json.read())
+			old_json.close()
+			new_json.close()
 		new_json = open(new_json_name, 'w')
+		new_json.write(dl_json)
 		new_json.close()
-		new_json = open(new_json_name, 'r')
-	if new_json:
-		old_json = open(old_json_name, 'w')
-		old_json.write(new_json.read())
-		old_json.close()
-		new_json.close()
-	new_json = open(new_json_name, 'w')
-	new_json.write(dl_json)
-	new_json.close()
-else:
-	print ("[error][json] Old file used. Please check your server connection / json creation")
+	else:
+		print ("[error][json] Old file used. Please check your server connection / json creation")
